bbc2c89c7b8a7f23ac26a7fcbe773d16878aaf74
kashimaa <kashimaahmed@gmail.com>
2024-11-02 23:19:29 +0300

    init

--- pytestmark.py
+++ pytestmark.py
@@ -9,7 +9,7 @@ async def test_query_1(engine: Engine) -> None:
     async with engine.connect() as conn:
         res = await conn.execute(text(TASK_1_QUERY))
 
-    # Сравнение полученных результатов с ожидаемыми
+   
     assert res.all() == [
         ("PG0235", datetime.timedelta(seconds=1500)),
         ("PG0234", datetime.timedelta(seconds=1500)),
